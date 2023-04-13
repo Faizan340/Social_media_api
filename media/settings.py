ee13ecde68eb8f5b4545053b4386539ffefaedfe
FaizanRashid007 <fznrsd@DESKTOP-DRD6N6C.(MINGW64)git config --global user.name Faizan>
2023-04-13 22:16:29 +0530

    Implemented APIs

--- media/settings.py
+++ media/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,6 +92,9 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -125,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
